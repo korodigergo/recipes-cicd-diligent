e22b3ef24c0867361e19461e2a10273980400b54
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-09-30 19:28:50 +0200

    have correct base in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rescipes-cicd-diligent/',
+  base: '/recipes-cicd-diligent/',
   server: {
     proxy: {
       '/api': {
