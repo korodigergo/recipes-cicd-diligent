d87310809e76d49d8e807e542ee66eb786e18fc1
korodigergo <127103460+korodigergo@users.noreply.github.com>
2024-09-30 19:51:18 +0200

    have issues trying to fix v10

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/recipes-cicd-diligent/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080/',
-        changeOrigin: true,
-      }
-    }
-  },
   plugins: [react()],
 });
